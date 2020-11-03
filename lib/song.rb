4f2a695060914357a280a106ea153381b3971598
erikn775 <erikn775@gmail.com>
2020-11-03 14:44:22 -0700

    commit tree 49ce648fada1f340d659fef0a229fe8ac4642769

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,10 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize
+    
+  end
+
   def self.all
     @@all
   end
@@ -10,4 +14,20 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+      song = self.new
+      song.name = name
+      @@all << song
+    end
+
+  def self.new_by_name(name)
+    @@all.each{|song| song.name }
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
 end
+
+Song.create("Modern Misery")
+#modern_misery.save
